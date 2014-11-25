e3bb60b9ea8317b1c611dd65a4f54f5390e4dceb
GZep <GZep@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-25 12:42:46 +0000

    Reduced code size
    git-svn-id: https://plugins.svn.wordpress.org/site-private/trunk@1032483 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Plugin Name ===
 Contributors: GZep
-Donate link: http://gzep.ru/
+Donate link: http://gzep.ru/donate/
 Tags: private, authorize
 Requires at least: 4.0
-Tested up to: 4.0
+Tested up to: 4.0.1
 Stable tag: trunk
 License: MIT
 License URI: http://opensource.org/licenses/MIT
--- site-private.php
+++ site-private.php
@@ -3,43 +3,21 @@
 Plugin Name: Site Private
 Plugin URI: http://gzep.ru/site-private-wordpress-plugin/
 Description: Simplest plugin that redirects unauthorized users to login page.
-Version: 1.0
+Version: 1.1
 Author: Gaiaz Iusipov
 Author URI: http://gzep.ru
 */
 
-defined('ABSPATH') or exit();
+defined('ABSPATH') or exit;
 
-class SitePrivate
+function siteprivate_redirect()
 {
-    private static $instance;
-
-    private function __construct()
-    {
-        add_action('template_redirect', array($this, 'actionRedirect'));
-    }
-
-    private function __clone()
-    {
-    }
-
-    public static function getInstance()
-    {
-        if (null === self::$instance) {
-            self::$instance = new self;
-        }
-        return self::$instance;
-    }
-
-    public function actionRedirect()
-    {
-        global $pagenow;
-        if ('wp-login.php' !== $pagenow
-            && !is_user_logged_in()
-            && !is_user_member_of_blog()) {
-            auth_redirect();
-        }
+    global $pagenow;
+    if ('wp-login.php' !== $pagenow
+        && !is_user_logged_in()
+        && !is_user_member_of_blog()) {
+        auth_redirect();
     }
 }
 
-add_action('plugins_loaded', 'SitePrivate::getInstance');
+add_action('plugins_loaded', 'siteprivate_redirect');
